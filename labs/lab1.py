e6ebcdf87dd3f3cfb920bd3cd8157c35f2af70e7
bondL75 <144202246+bondL75@users.noreply.github.com>
2023-09-27 11:20:57 +0500

    Написала начало

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,1 +1,16 @@
+
 from math import *
+
+def task1():
+    x = float(input("введите x: "))
+    print(input(y))
+    print(input(z))
+
+    a = (abs(x-1)**-1)-(abs(y)**-1) - (1 + (x**2)/2 + (y**2)/4)
+    b = x * (tan(z)+5)
+
+    print("a= {:.4f}, b= {:.4f}")
+
+
+
+task1()
