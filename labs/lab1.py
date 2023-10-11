7a7b750503146c4acbcbee88bacf01372e2e3be0
bondL75 <144202246+bondL75@users.noreply.github.com>
2023-10-11 11:13:10 +0500

    начала до 6 задания

--- labs/lab1.py
+++ labs/lab1.py
@@ -3,8 +3,8 @@ from math import *
 
 def task1():
     x = float(input("введите x: "))
-    print(input(y))
-    print(input(z))
+    y = float(input("введите y: "))
+    z = float(input("введите z: "))
 
     a = (abs(x-1)**-1)-(abs(y)**-1) - (1 + (x**2)/2 + (y**2)/4)
     b = x * (tan(z)+5)
@@ -12,5 +12,39 @@ def task1():
     print("a= {:.4f}, b= {:.4f}")
 
 
+def task2():
+    x = float(input("введите x: "))
+    a = 2
+    b = 2
+    c = -5
 
-task1()
+    f = (x**a) + (b*x**1/3)/(c*x + a)
+
+    print("f(x)= {:.4f}")
+
+
+def task3():
+    x = float(input("введите x: "))
+
+    f = (sin(x) + cos(x))/(cos(x) - sin(x))*tan(x)
+
+    print("f(x)= {:.4f}")
+
+
+def task4():
+    a = float(input("введите a: "))
+    b = float(input("введите b: "))
+    c = float(input("введите c: "))
+
+    print("x= {:.4f}")
+
+
+def task5():
+    pass
+
+
+def task6():
+    V = float(input("введите объём шара: "))
+    P = V*6/
+
+task3()
